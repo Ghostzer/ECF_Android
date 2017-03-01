4d27763763f23e4a04054e9c2a2808d655a6a6b8
Afpa <Afpa>
2017-03-01 17:05:41 +0100

    fixes

--- API/webServiceECF/src/routes.php
+++ API/webServiceECF/src/routes.php
@@ -6,17 +6,42 @@ $app->group('/api', function () use ($app) {
   $app->get('/regates',function ($request, $response, $args) {
        $sth = $this->db->prepare("SELECT * FROM regate");
       $sth->execute();
-      $movies = $sth->fetchAll();
-      return $this->response->withJson($movies);
+      $regates = $sth->fetchAll();
+      return $this->response->withJson($regates);
   });
 
   //get regate by id
-  $app->get('/regate/[{id}]', function ($request, $response, $args) {
-         $sth = $this->db->prepare("SELECT * FROM regate");
+  $app->get('/regatebyid/[{id}]', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM regate reg INNER JOIN commissaire com ON reg.id_commissaire=com.id_commissaire INNER JOIN personne per ON com.id_personne=per.id_personne WHERE id_regate=:id");
 		 $sth->bindParam("id", $args['id']);
         $sth->execute();
-        $moviesById = $sth->fetchObject();
-        return $this->response->withJson($moviesById);
+        $regatesById = $sth->fetchObject();
+        return $this->response->withJson($regatesById);
+    });
+	
+	//get regate by winter
+  $app->get('/regates/winter',function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM regate reg INNER JOIN Challenge cha ON reg.id_challenge=cha.id_challenge WHERE cha.id_challenge = 1");
+      $sth->execute();
+      $regatesByWinter = $sth->fetchAll();
+      return $this->response->withJson($regatesByWinter);
+    });
+	
+	//get regate by summer
+  $app->get('/regate/summer',function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM regate reg INNER JOIN Challenge cha ON reg.id_challenge=cha.id_challenge WHERE cha.id_challenge = 2");
+      $sth->execute();
+      $regatesBySummer = $sth->fetchAll();
+      return $this->response->withJson($regatesBySummer);
+    });
+	
+	//get scores by regate
+	  $app->get('/resultatsbyregate/[{id}]', function ($request, $response, $args) {
+         $sth = $this->db->prepare("SELECT * FROM regate reg INNER JOIN participer par ON reg.id_regate=par.id_regate INNER JOIN Voilier voi ON par.id_voilier=voi.id_voilier WHERE reg.id_regate=:id");
+		 $sth->bindParam("id", $args['id']);
+      $sth->execute();
+      $regates = $sth->fetchAll();
+      return $this->response->withJson($regates);
     });
 
       // Add a new todo
