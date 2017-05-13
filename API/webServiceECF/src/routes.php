e59da4b7164f9b07c5b228b4db3d3a8638ee7c79
ghostzer <djxrico@gmail.com>
2017-05-13 16:09:56 +0200

    fix route

--- API/webServiceECF/src/routes.php
+++ API/webServiceECF/src/routes.php
@@ -46,54 +46,5 @@ INNER JOIN commissaire com ON reg.id_commissaire=com.id_commissaire INNER JOIN p
       return $this->response->withJson($regates);
     });
 
-      // Add a new todo
-    $app->post('/todo', function ($request, $response) {
-        $todo = json_decode($request->getBody());
-        $sql = "INSERT INTO task (task, priority) VALUES (:task, :priority)";
-        $sth = $this->db->prepare($sql);
-        $sth->bindParam("task", $todo->task);
-        $sth->bindParam("priority", $todo->priority);
-        $sth->execute();
-        $todo->id=$this->db->lastInsertId();
-        //$input['id'] = $this->db->lastInsertId();
-        //$this->response->withStatus(201);
-        return $this->response->withJson($todo)->withStatus(201);
-    });
-
-
-    // DELETE a todo with given id
-    $app->delete('/todo/[{id}]', function ($request, $response, $args) {
-         $sth = $this->db->prepare("DELETE FROM task WHERE id=:id");
-        $sth->bindParam("id", $args['id']);
-        $sth->execute();
-        return $this->response->withStatus(204); //no-content
-    });
-
-
-// Update todo with given id
-    $app->put('/todo/[{id}]', function ($request, $response, $args) {
-        //$input = $request->getParsedBody();
-        //$this->logger->debug($request->getBody());
-        $todo = json_decode($request->getBody());
-        $sql = "UPDATE task SET task=:task,priority=:priority  WHERE id=:id";
-         $sth = $this->db->prepare($sql);
-        $sth->bindParam("id", $args['id']);
-        $sth->bindParam("task", $todo->task);
-        $sth->bindParam("priority", $todo->priority);
-        $sth->execute();
-
-        //load complete oci_fetch_object
-       $sth = $this->db->prepare("SELECT * FROM task WHERE id=:id");
-       $sth->bindParam("id", $args['id']);
-       $sth->execute();
-       $todo = $sth->fetchObject();
-
-        //$input['id'] = $args['id'];
-        return $this->response->withJson($todo);
-    });
-
-
-
-
 
 });
